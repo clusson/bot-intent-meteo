000ca94dc5ee5f1d8a8439a248eb6080cca50969
Clément Lusson <clementlusson@gmail.com>
2017-10-17 14:53:19 +0200

    set weather in message

--- src/index.js
+++ src/index.js
@@ -2,29 +2,29 @@ import { connexion } from './rabbitConnexion'
 import { assertQueue, sendTo } from './rabbitUtils'
 const connexionEstablished = connexion()
 let weather = require("Openweather-Node")
-let fs = require('fs');
+let fs = require('fs')
 let city
+let weatherMessage
 
 //set your API key if you have one
-weather.setAPPID(process.env.VERIFY_TOKEN)
+weather.setAPPID(process.env.PAGE_ACCESS_TOKEN)
 //set the culture
 weather.setCulture("fr")
 //set the forecast type
 weather.setForecastType("daily") //or "" for 3 hours forecast
 
-
-weather.now(city, function (err, aData) {
-  if (err) console.log(err)
-  else {
-    console.log(aData.getDegreeTemp())
-  }
-})
-
 assertQueue(connexionEstablished, ($message) => {
-  const newMessage = Object.assign($message.message, { content: repartie })
+
+  console.log($message)
+
+  weather.now($message.message, function (err, aData) {
+    if (err) console.log(err)
+    else {
+      console.log(aData.getDegreeTemp())
+      weatherMessage = aData.getDegreeTemp()
+    }
+  })
+  const newMessage = Object.assign($message.message, { content: weatherMessage })
   const newIntentMessage = Object.assign($message, { message: newMessage })
   sendTo(connexionEstablished, JSON.stringify(newIntentMessage))
 })
-
-
-
